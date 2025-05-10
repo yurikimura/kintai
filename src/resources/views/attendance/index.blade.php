11cc57a38fdc8c010a9a9c75018b1ff5d5266f99
yurikimura <yuri.76135@gmail.com>
2025-05-10 14:05:42 +0900

    [お疲れ様でした]というメッセージをずっと表示させる

--- src/resources/views/attendance/index.blade.php
+++ src/resources/views/attendance/index.blade.php
@@ -94,7 +94,7 @@
             <button class="break-button {{ $attendance && $attendance->working_status === 'working' ? '' : 'hidden' }}" id="startBreak">休憩入</button>
             <button class="break-return-button {{ $attendance && $attendance->working_status === 'on_break' ? '' : 'hidden' }}" id="endBreak">休憩戻</button>
         </div>
-        <div class="thank-you-message hidden" id="thankYouMessage">
+        <div class="thank-you-message show" id="thankYouMessage">
             お疲れ様でした。
         </div>
     </div>
